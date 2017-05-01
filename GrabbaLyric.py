517abf8c99f501ddc8d6dd47af7c4ca7d56eeac0
Mark Magree <MarkMagree@gmail.com>
2017-05-01 11:34:03 -0400

    now downloads search page

--- GrabbaLyric.py
+++ GrabbaLyric.py
@@ -24,17 +24,37 @@ def split_options(args):
     else:
         return args[1:], options
 
-#TODO '+'.join(search) isn't putting the '+' in there
 def retrieve_data(search,opts):
-    surl = "search.azlyrics.com/search.php?q=" + '+'.join(search)
+    surl = "http://search.azlyrics.com/search.php?q=" +\
+            '+'.join(search)
     if opts["verbose"]:
         print(surl)
+    res = requests.get(surl)
+    res.raise_for_status()
+    if opts["verbose"]:
+        print(res.text)
+    return res
 
+def parse_data(html,opts):
+    #TODO use bs4 to return lyrics
+    pass
+
+def save_to_cache_file(text,srch,opts):
+    #TODO save lyrics to a file somewhere 
+    pass
+
+def check_for_cached_lyrics(search,opts):
+    #TODO check cache file for lyrics before downlodaing
+    pass
 
 def main(args):
     if not is_arguments(args):
         sys.exit()
     search,options = split_options(args)
-    retrieve_data(search,options)
+    check_for_cached_lyrics(search,options)
+    data = retrieve_data(search,options)
+    text = parse_data(data,options)
+    save_to_cache_file(text,search,options)
+    print(text)
 
 main(sys.argv)
